7a14e9584507f5086dbbb68728b68480180028ae
rizalayub <47585805+rizalayub@users.noreply.github.com>
2019-03-04 11:33:58 +0800

    Add files via upload

--- SeoAnalyser.Tests/Controllers/HomeControllerTest.cs
+++ SeoAnalyser.Tests/Controllers/HomeControllerTest.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SeoAnalyser;
 using SeoAnalyser.Controllers;
+using SeoAnalyser.Models;
+using SeoAnalyser.Services;
 
 namespace SeoAnalyser.Tests.Controllers
 {
@@ -34,9 +36,54 @@ namespace SeoAnalyser.Tests.Controllers
 
             // Act
             ViewResult result = controller.Contact() as ViewResult;
-
+            
             // Assert
             Assert.IsNotNull(result);
+        }
+
+        [DataTestMethod]
+        [DataRow("http://www.google.com")]
+        public void FilterStopWord_URL(string value)
+        {
+            StopWords model = new StopWords();
+            ISeoFilter seoFilter = new StopWordUrlFilterService();
+            IEnumerable<StopWords> resultLink = new List<StopWords>();
+
+            string response = seoFilter.GetDataFromSource(value);
+            var res = seoFilter.GetResponseBody(response);
+            var result = seoFilter.CalculateStopCount(res);
+            var resultBody = model.PopulateStopWords(result);
+
+            Assert.IsNotNull(resultBody,"Stop Words is available");
+        }
+
+        [DataTestMethod]
+        [DataRow("and and my name is rizal ayub or muhamad rizal, I like to work as software developer")]
+        public void FilterStopWord_Text(string value)
+        {
+            StopWords model = new StopWords();
+            ISeoFilter seoFilter = new StopWordTextFilterService();
+            IEnumerable<StopWords> resultLink = new List<StopWords>();
+
+            string response = seoFilter.GetDataFromSource(value);
+            var res = seoFilter.GetResponseBody(response);
+            var result = seoFilter.CalculateStopCount(res);
+            var resultBody = model.PopulateStopWords(result);
+
+            var body = resultBody.FirstOrDefault().Body;
+
+            Assert.IsTrue(resultBody.Any( c => c.Word == "and" && c.Count==2) , "Word 'and' is 2 count");
+        }
+
+
+        private static FormCollection CreateFormCollection(string data)
+        {
+
+            FormCollection form = new FormCollection();
+
+            form.Add("url", data);
+
+            return form;
         }
     }
 }
